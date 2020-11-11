97021e464e6f9d4ba0921492d2dced3d1136ec11
James  Wallace <jameswallace@Jamess-MBP.lan1>
2020-11-11 12:34:34 -0500

    bastion host working

--- main.tf
+++ main.tf
@@ -12,11 +12,39 @@ provider "aws" {
 }
 
 resource "aws_vpc" "dev_east" {
-  cidr_block = "1.0.0.0/24"
+  cidr_block           = "1.0.0.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = "Terraform Dev East"
   }
+}
+
+resource "aws_internet_gateway" "dev_east_igw" {
+  vpc_id = aws_vpc.dev_east.id
+
+  tags = {
+    Name = "Terraform Dev East Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "dev_east_rt" {
+  vpc_id = aws_vpc.dev_east.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev_east_igw.id
+  }
+
+  tags = {
+    Name = "Terraform Dev East Route Table"
+  }
+}
+
+resource "aws_main_route_table_association" "dev_east_rt_main" {
+  vpc_id         = aws_vpc.dev_east.id
+  route_table_id = aws_route_table.dev_east_rt.id
 }
 
 resource "aws_subnet" "dev_east_az1" {
@@ -49,11 +77,35 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allows inbound SSH traffic"
+  vpc_id      = aws_vpc.dev_east.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Outbound traffic to anywhere"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "bastion_host" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.micro"
-  key_name      = "Terraform Dev East Key Pair"
-  subnet_id     = aws_subnet.dev_east_az1.id
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  key_name                    = "Terraform Dev East Key Pair"
+  subnet_id                   = aws_subnet.dev_east_az1.id
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
 
   tags = {
     Name = "Terraform Bastion Host"
